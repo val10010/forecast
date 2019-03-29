f4d638a9b9b5f23097c9bf739b83f8916a21a8a4
Valentyn <valantine10010@gmail.com>
2019-03-29 13:30:57 +0200

    Updates

--- precache-manifest.5c6ecc502730b36a5d25d4d047644c11.js
+++ precache-manifest.5c6ecc502730b36a5d25d4d047644c11.js
@@ -12,19 +12,19 @@ self.__precacheManifest = [
     "url": "/forecast/static/js/runtime~main.1c70b54c.js"
   },
   {
-    "revision": "5c02bfa68506f180e868",
-    "url": "/forecast/static/js/main.7cac3ba2.chunk.js"
+    "revision": "bc1c7fc33adf799d7ef3",
+    "url": "/forecast/static/js/main.3d81375a.chunk.js"
   },
   {
     "revision": "ecd1370644ff2a430bcf",
     "url": "/forecast/static/js/2.c2966d67.chunk.js"
   },
   {
-    "revision": "5c02bfa68506f180e868",
+    "revision": "bc1c7fc33adf799d7ef3",
     "url": "/forecast/static/css/main.080a90a6.chunk.css"
   },
   {
-    "revision": "e89f93a714f388c6345fdb8eca929317",
+    "revision": "507195cb5c2ac2fc1f7f62ced634dbdc",
     "url": "/forecast/index.html"
   }
 ];
